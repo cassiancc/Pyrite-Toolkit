a7becc57dda49bbd60b43303f2a0c76811fba121
Cassian Godsted <cassian@godsted.com>
2024-12-08 17:27:07 -0500

    Fixes for property files and QOL logs

--- standalones/pack-repair.js
+++ standalones/pack-repair.js
@@ -1,10 +1,9 @@
 var fs = require('fs');
 const path = require('path');
 
-const dir = process.argv[2]
-
 let badPaths = []
 let badDirs = []
+let fixedFiles = 0
 
 var isWin = process.platform === "win32";
 var assets = "assets/"
@@ -12,7 +11,6 @@ var assets = "assets/"
 if (isWin) {
     assets = "assets\\"
 }
-
 
 async function* walk(dir) {
     for await (const d of await fs.promises.opendir(dir)) {
@@ -29,7 +27,32 @@ async function* walk(dir) {
 }
 
 // Then, use it with a simple async for loop
-async function main() {
+async function main(dir) {
+    if (dir == undefined) {
+        console.error("Please provide a path to the unzipped resource pack you wish to repair as a command line argument.")
+        console.error("For example: node standalones/pack-repair \"/home/username/Minecraft/resourcepacks/resourcepackname\"")
+        return
+    }
+    repair(dir)
+    // repair(dir)
+    
+    console.info("Resource Pack repair finished, " + fixedFiles + " files with broken paths repaired.")
+}
+
+function repairPath(path) {
+    let pathArray = path.split(assets)
+    let badDir = pathArray[1].split("/")
+    badDir = badDir[badDir.length - 1]
+    pathArray[1] = pathArray[1].replaceAll(" ", "_").toLowerCase()
+    let newPath = pathArray[0] + assets + pathArray[1]
+
+    // console.log(badDir)
+    badDirs.push(badDir)
+    fs.renameSync(path, newPath)
+    fixedFiles++
+}
+
+async function repair(dir) {
     for await (const p of walk(dir)) {
         try {
             let pathArray = p.split(assets)
@@ -40,7 +63,9 @@ async function main() {
     
             // console.log(badFile)
             badDirs.push(badFile)
+            console.log(badFile)
             fs.renameSync(p, newPath)
+            fixedFiles++
         }
         catch {}
         
@@ -56,6 +81,7 @@ async function main() {
         // console.log(badDir)
         badDirs.push(badDir)
         fs.renameSync(badPath, newPath)
+        fixedFiles++
     })
     for await (const p of walk(dir)) {
         if (p.includes(".properties")) {
@@ -63,10 +89,12 @@ async function main() {
                 let split = contents.split("model=")
                 if (split != undefined) {
                     try {
-                        let split1 = split[1].replace(" ", "_").toLowerCase()
+                        let split1 = split[1].replaceAll(" ", "_").toLowerCase()
                         let newContents = split[0] + "model=" + split1
-                        if (split1 != split[1])
-                            fs.writeFile(p, newContents, function (err) { if (err) throw err; });
+                        if (split1 != split[1]) {
+                            fs.writeFile(p, newContents, function (err) { if (err) throw err; }); 
+                            fixedFiles++
+                        }
                     }
                     catch {
 
@@ -90,8 +118,10 @@ async function main() {
                     });
                     jContents.textures = texture
     
-                    if (jContents != undefined)
-                        fs.writeFile(p, JSON.stringify( jContents), function (err) { if (err) throw err; });
+                    if (jContents != undefined) {
+                        fs.writeFile(p, JSON.stringify( jContents, null, " "), function (err) { if (err) throw err; }); 
+                        fixedFiles++
+                    }
                 }
                 
                 
@@ -103,4 +133,4 @@ async function main() {
     }
 }
 
-main()
+main(process.argv[2])
